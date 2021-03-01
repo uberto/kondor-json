6e68e6840bfa4cccf2c36e24e91b354e120c9d00
uberto <uberto.gama@gmail.com>
2021-03-01 14:18:31 +0000

    version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/com.ubertob.kondor/kondor-core/badge.svg?style=plastic)](https://maven-badges.herokuapp.com/maven-central/com.ubertob.kondor/kondor-core)
+
+
 # KondorJson
 A library to serialize/deserialize Json fast and safely without reflection or generators
+
+
+## Dependency declaration
+Maven
+```
+<dependency>
+  <groupId>com.ubertob.kondor</groupId>
+  <artifactId>kondor-core</artifactId>
+  <version>1.0.0</version>
+</dependency>
+```
+
+Gradle
+```
+testImplementation 'com.ubertob.kondor:kondor-core:1.0.0'
+```
--- scripts/release-on-mavencentral.sh
+++ scripts/release-on-mavencentral.sh
@@ -1,9 +1,14 @@
+# Steps:
+# verify it's all working with a ./gradlew clean build
+# remove snapshot from the version manually in build.gradle
+# update the README.md
+# check the CHANGELOG.md
+# launch:
+
 ./gradlew uploadArchives -p kondor-core
 
-
-# before launching it you need to update the version manually (remove snapshot)
-# and then bouncing the version with SNAPSHOT after
 # then go to sonatype site and login
 # https://oss.sonatype.org/#nexus-search;quick~pesticide
-# select Staging Repositories, and close the corresponding one.
-# then click release and wait 10 min
+# select Staging Repositories, and close the corresponding one (empty desc is fine)1.
+# then click release and wait ~10 min to be able to download it
+# and then bouncing the version with SNAPSHOT in build.gradle
