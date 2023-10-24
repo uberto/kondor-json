71e9baa8d775fc84c1d69135bb7f5af604a53578
Uberto Barbini <uberto.gama@gmail.com>
2023-10-24 18:42:49 +0900

    Added jmh benchmark for comparison with other Json libraries
    Signed-off-by: Uberto Barbini <uberto.gama@gmail.com>

---
+++ kondor-examples/src/jmh/kotlin/com/ubertob/kondor/json/jmh/DemoClass.kt
@@ -0,0 +1,27 @@
+package com.ubertob.kondor.json.jmh
+
+import kotlin.random.Random
+
+data class DemoClass(
+    val text: String,
+    val boolean: Boolean,
+    val double: Double,
+    val nullableInt: Int?,
+    val array: List<String>
+) {
+    companion object {
+        fun random() = DemoClass(
+            text = randomString(),
+            boolean = Random.nextBoolean(),
+            double = Random.nextDouble(),
+            nullableInt = if (Random.nextBoolean()) Random.nextInt() else null,
+            array = (1..Random.nextInt(5, 20)).map { randomString() }.toList()
+        )
+    }
+}
+
+val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+fun randomString(): String =
+    (1..Random.nextInt(3, 10))
+        .map { charPool.random() }
+        .joinToString(separator = "")
