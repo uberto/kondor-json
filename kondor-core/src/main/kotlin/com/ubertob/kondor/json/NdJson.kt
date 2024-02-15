5e8c67a1f8f7108fd34cefdde26dc5967c3fcc32
uberto <uberto.gama@gmail.com>
2024-02-15 21:27:26 +0900

    adding utility methods to handle NdJson

---
+++ kondor-core/src/main/kotlin/com/ubertob/kondor/json/NdJson.kt
@@ -0,0 +1,34 @@
+package com.ubertob.kondor.json
+
+import com.ubertob.kondor.outcome.traverse
+import java.io.InputStream
+import java.io.OutputStream
+import kotlin.text.Charsets.UTF_8
+
+fun <T : Any> fromNdJson(converter: JConverter<T>): (Sequence<String>) -> JsonOutcome<List<T>> =
+    { lines ->
+        lines.traverse { converter.fromJson(it) }
+    }
+
+fun <T : Any> toNdJson(converter: JConverter<T>): (Iterable<T>) -> Sequence<String> =
+    { coll ->
+        coll.asSequence().map { converter.toJson(it) }
+    }
+
+
+fun <T : Any> fromNdJsonStream(converter: JConverter<T>): (InputStream) -> JsonOutcome<List<T>> =
+    { stream ->
+        stream.bufferedReader().lineSequence().traverse { converter.fromJson(it) }
+    }
+
+fun <T : Any> toNdJsonStream(converter: JConverter<T>): (Iterable<T>, OutputStream) -> Unit =
+    { coll, stream ->
+
+
+            coll.onEach {
+                val json = converter.toJson(it)
+                stream.write(json.toByteArray(UTF_8))
+                stream.write('\n'.code)
+            }
+
+    }
