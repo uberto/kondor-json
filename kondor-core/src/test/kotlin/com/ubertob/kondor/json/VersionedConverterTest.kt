6d66f51ee4644add73abf65819b9ceac3ebe5fce
uberto <uberto.gama@gmail.com>
2024-04-12 14:49:53 +0900

    changed VersionedConverterTest assertions in Strikt
    (cherry picked from commit 8d1c87efb6293588eb690ac888ee7f9002b1fe31)

--- kondor-core/src/test/kotlin/com/ubertob/kondor/json/VersionedConverterTest.kt
+++ kondor-core/src/test/kotlin/com/ubertob/kondor/json/VersionedConverterTest.kt
@@ -1,11 +1,15 @@
 package com.ubertob.kondor.json
 
+import com.ubertob.kondor.json.JsonStyle.Companion.compactSorted
 import com.ubertob.kondor.json.jsonnode.JsonNodeObject
 import com.ubertob.kondor.json.jsonnode.NodePathRoot
+import com.ubertob.kondor.randomText
 import com.ubertob.kondortools.expectFailure
 import com.ubertob.kondortools.expectSuccess
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import kotlin.random.Random
 
 
 private data class Example(
@@ -16,7 +20,7 @@ private data class Example(
 private object V1Format : JAny<Example>() {
     val i by num(Example::i)
     val s by str(Example::s)
-    
+
     override fun JsonNodeObject.deserializeOrThrow(): Example =
         Example(i = +i, s = +s)
 }
@@ -24,7 +28,7 @@ private object V1Format : JAny<Example>() {
 private object V2Format : JAny<Example>() {
     val int by num(Example::i)
     val str by str(Example::s)
-    
+
     override fun JsonNodeObject.deserializeOrThrow(): Example =
         Example(i = +int, s = +str)
 }
@@ -40,57 +44,61 @@ private val converterWithDefaultVersion = VersionMapConverter(
 
 
 class VersionedConverterTest {
-    companion object {
-        val testStyle = JsonStyle.compact.copy(sortedObjectFields = true)
-    }
-    
+
     @Test
     fun `reads multiple versions, writes one version`() {
         val converter = converterWithMandatoryVersionField
-        
+
         val original = Example(2, "eyes")
-        
+
         val v1Json = """{"@version":"1","i":2,"s":"eyes"}"""
         val v2Json = """{"@version":"2","int":2,"str":"eyes"}"""
-        
-        assertEquals(original, converter.fromJson(v1Json).expectSuccess())
-        assertEquals(original, converter.fromJson(v2Json).expectSuccess())
-        
-        val generatedJson = converter.toJson(original, testStyle)
-        assertEquals(v2Json, generatedJson)
+
+        expectThat(original).isEqualTo(converter.fromJson(v1Json).expectSuccess())
+        expectThat(original).isEqualTo(converter.fromJson(v2Json).expectSuccess())
+
+        val generatedJson = converter.toJson(original, compactSorted)
+        expectThat(v2Json).isEqualTo(generatedJson)
     }
-    
+
     @Test
     fun `fails when mandatory version is missing`() {
         val converter = converterWithMandatoryVersionField
-        
+
         val jsonWithoutSpecificVersion = """{"i":3,"s":"amigos"}"""
-        
+
         val failure = converter.fromJson(jsonWithoutSpecificVersion).expectFailure() as JsonPropertyError
-        assertEquals(NodePathRoot, failure.path)
-        assertEquals(converter.versionProperty, failure.propertyName)
+        expectThat(failure.path).isEqualTo(NodePathRoot)
+        expectThat(converter.versionProperty).isEqualTo(failure.propertyName)
     }
-    
+
     @Test
     fun `can use default version if not specified in the json`() {
         val converter = converterWithDefaultVersion
-        
+
         val badJson = """{"i":3,"s":"amigos"}"""
-        
+
         val parsed = converter.fromJson(badJson).expectSuccess()
-        assertEquals((Example(3, "amigos")), parsed)
+        val expected = Example(3, "amigos")
+        expectThat(expected).isEqualTo(parsed)
     }
-    
+
     @Test
     fun `generates json node equivalent to json text`() {
         val converter = converterWithMandatoryVersionField
-        val original = Example(4, "seasons")
-        
-        val directJson =
-            converter.toJson(original, testStyle)
-        val jsonFromJsonNode =
-            JJsonNode.toJson(converter.toJsonNode(original, NodePathRoot), testStyle)
-        
-        assertEquals(directJson, jsonFromJsonNode)
+
+        repeat(10) {
+            val original = Example(Random.nextInt(), randomText(10))
+
+            val directJson = converter.toJson(original, compactSorted)
+
+            val parsed = converter.fromJson(directJson).expectSuccess()
+            expectThat(parsed).isEqualTo(original)
+
+            val jsonFromJsonNode =
+                JJsonNode.toJson(converter.toJsonNode(original, NodePathRoot), compactSorted)
+
+            expectThat(directJson).isEqualTo(jsonFromJsonNode)
+        }
     }
 }
