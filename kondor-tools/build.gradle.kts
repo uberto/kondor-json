f3f95b16de5825c707af934133dd77b8a07fe083
Chriptus <amcmmm13@hotmail.com>
2024-04-27 12:32:27 +0100

    Use Kotlin for Gradle scripts and Gradle Version Catalogs for setting the dependencies.

---
+++ kondor-tools/build.gradle.kts
@@ -0,0 +1,79 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    `common-kotlin`
+    id("maven-publish")
+    id("signing")
+}
+
+tasks.withType<GenerateModuleMetadata> {
+    enabled = false
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+dependencies {
+    implementation(project(":kondor-core"))
+    api(libs.jUnit.api)
+
+    implementation(libs.kotlin.jdk8)
+    implementation(libs.kotlin.reflect) //needed for source generator
+    implementation(libs.kotlinPoet)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components.getByName("java"))
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            pom {
+                name = "kondor-tools"
+                description = "A suite of tools to work with Kondor json library"
+                url = "https://github.com/uberto/kondor-json"
+                inceptionYear = "2021"
+                scm {
+                    url = "https://github.com/uberto/kondor-json"
+                    connection = "https://github.com/uberto/kondor-json.git"
+                    developerConnection = "git@github.com:uberto/kondor-json.git"
+                }
+                licenses {
+                    license {
+                        name = "The Apache Software License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                        distribution = "repo"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "UbertoBarbini"
+                        name = "Uberto Barbini"
+                        email = "uberto.gama@gmail.com"
+                    }
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "OSSRH"
+            uri(
+                if (version.toString().endsWith("SNAPSHOT")) "https://oss.sonatype.org/content/repositories/snapshots/"
+                else "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+            )
+            credentials {
+                username = findProperty("nexusUsername").toString()
+                password = findProperty("nexusPassword").toString()
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications.getByName("mavenJava"))
+}
