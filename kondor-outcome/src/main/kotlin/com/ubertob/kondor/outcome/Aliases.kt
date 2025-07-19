1398e62b0b97a89055f5683a5252d3930def8f97
Uberto Barbini <uberto.gama@gmail.com>
2025-07-19 12:46:28 +0900

    added documentation to Outcome module and missing tests
    Signed-off-by: Uberto Barbini <uberto.gama@gmail.com>

--- kondor-outcome/src/main/kotlin/com/ubertob/kondor/outcome/Aliases.kt
+++ kondor-outcome/src/main/kotlin/com/ubertob/kondor/outcome/Aliases.kt
@@ -1,5 +1,15 @@
 package com.ubertob.kondor.outcome
 
+/**
+ * Represents an outcome that doesn't return a value (only success or failure).
+ * This is a convenience alias for [BaseOutcome] with [Unit] as the success type.
+ */
 typealias UnitOutcome = BaseOutcome<Unit>
 
+/**
+ * Represents a standard outcome with [OutcomeError] as the error type.
+ * This is a convenience alias for [Outcome] with [OutcomeError] as the error type.
+ *
+ * @param T The type of the success value
+ */
 typealias BaseOutcome<T> = Outcome<OutcomeError, T>
