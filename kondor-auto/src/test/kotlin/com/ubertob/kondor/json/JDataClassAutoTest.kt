dd0145f3f148ef728b44efcd26c6dd024cfcb1bd
Uberto Barbini <uberto.barbini@kraken.tech>
2025-10-01 14:44:04 +0100

    clean up test

--- kondor-auto/src/test/kotlin/com/ubertob/kondor/json/JDataClassAutoTest.kt
+++ kondor-auto/src/test/kotlin/com/ubertob/kondor/json/JDataClassAutoTest.kt
@@ -1,16 +1,33 @@
 package com.ubertob.kondor.json
 
+import com.ubertob.kondortools.expectSuccess
 import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
 
 class JDataClassAutoTest {
 
-    object PersonRefl : JDataClassAuto<Person>(Person::class)
+    object JPersonAuto : JDataClassAuto<Person>(Person::class)
+
 
     @Test
     fun `JDataClassAuto doesn't need the fields declaration`() {
-        PersonRefl.toJson(randomPerson(), JsonStyle.prettyWithNulls)
 
-        PersonRefl.testParserAndRender(100) { randomPerson() }
+        val alice = Person(123, "Alice")
+
+        val json = JPersonAuto.toJson(alice)
+
+        val parsed = JPersonAuto.fromJson(json)
+
+        expectThat(parsed.expectSuccess()).isEqualTo(alice)
+
+    }
+
+    @Test
+    fun `JDataClassAuto handle nulls and random values`() {
+        JPersonAuto.toJson(randomPerson(), JsonStyle.prettyWithNulls)
+
+        JPersonAuto.testParserAndRender(100) { randomPerson() }
 
     }
 }
